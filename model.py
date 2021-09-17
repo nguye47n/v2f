7da3bf1d03fef6ac6b410df761b95a0557ac7954
nguye47n <57548683+nguye47n@users.noreply.github.com>
2021-09-17 15:25:12 -0400

    Update model.py

--- model.py
+++ model.py
@@ -38,7 +38,7 @@ def vgg16(fine_tuning=False, feature_extraction=False, weights=None):
 def lstm(weights=None, timesteps=None, mask_value=20):
 
     model = tf.keras.Sequential()
-    model.add(layers.Masking(mask_value, input_shape=(timesteps, 4096)))
+    model.add(layers.Masking(mask_value, input_shape=(timesteps, 3)))
     model.add(layers.LSTM(256, return_sequences=True))
     model.add(layers.Dropout(0.25))
     model.add(layers.LSTM(256, return_sequences=True))
