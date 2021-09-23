a6030bc6b4078d9d59aae0a4d4b0fd0c78f40605
nguye47n <57548683+nguye47n@users.noreply.github.com>
2021-09-22 23:21:41 -0400

    Update defines.py

--- defines.py
+++ defines.py
@@ -6,12 +6,13 @@ ACTIONS:
 2 - VGG16 feature extraction and LSTM training
 3 - VGG16 feature extraction and LSTM inference
 '''
-PARAM_ACTION = 1
+PARAM_ACTION = 3
 
 PARAM_BATCHES = 2
 PARAM_N_EPOCHS = 20
 PARAM_N_TESTS = 100
 PARAM_EPOCH_STEPS = 100
+PARAM_LEARNING_RATE = 1e-5
 
 PARAM_TIMESTEPS = 64
 
@@ -22,5 +23,6 @@ PARAM_PATH_IMAGES = '/Users/phuongnhi//Desktop/MHC/Summer21/research/dataset/tra
 PARAM_PATH_LABELS = '/Users/phuongnhi/Desktop/MHC/Summer21/research/dataset/training_labels/training_labels.npy'
 PARAM_WEIGHTS_PATH_1 = 'CNN.hdf5'
 PARAM_WEIGHTS_PATH_2 = 'RNN.hdf5'
+PARAM_PATH_TEST_NPY = 'results.npy'
 
 PARAM_METRICS = 'accuracy'								# TODO: monitor more metrics... look up the options.
